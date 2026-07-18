142c920a3faeb6af47cb9d034db647b1d4bcd802
Gleidson <gleidsonabstract.evgm@gmail.com>
2026-07-18 15:10:22 -0300

    correction main file

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,15 @@ int main(int argc, char **argv)
     }
 
     Image image;
-    image.perls = NULL; // Inicializa o ponteiro como nulo
 
-    int resultado = read_bmp(argv[1], &image);
+    int response_read = read_bmp(argv[1], &image);
     
-    if (resultado == 1) {
+    if (response_read == 1) {
         print_head_image(&image);
         print_matriz_image(&image, 3);
         write_bmp(argv[2], &image);
     } else {
-        printf("Erro ao ler o arquivo BMP (Erro codigo: %d).\n", resultado);
+        printf("Erro ao ler o arquivo BMP (Erro codigo: %d).\n", response_read);
     }
 
     return 0;
